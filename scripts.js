862c76a40d89ca19b806e1d67590aa59693074a9
Caio Senna <caiosenna93@gmail.com>
2022-01-23 16:18:42 -0300

    botao de submit

--- scripts.js
+++ scripts.js
@@ -18,26 +18,17 @@ document.getElementById('crypto-type').addEventListener('change',()=>{
     }
 });
 
-/*
 
-document.getElementsByName('radio').addEventListener('click',(event)=>{
-    
- 
-
-
+document.getElementById('radio1').addEventListener('click',()=>{
+    document.getElementById('botao').innerText='Criptografar!'
 });
 
-*/
+
+document.getElementById('radio2').addEventListener('click',()=>{
+    document.getElementById('botao').innerText='Descriptografar!'
+});
 
 
-
-
-
-function readRadio(){
-
-    document.getElementsByName('radio')
-
-};
 
 
 
@@ -46,15 +37,27 @@ function readRadio(){
 
 function criptoCesar(){
     let alfabeto=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-    let texto=document.getElementById('campo-input');
-    let inc=document.querySelector('.incremento[input]')
-    console.log(texto.value);
+    let texto=document.getElementById('campo-input').value;
+    let inc=parseInt(document.querySelector('#inc').value);
 
-
-
+    for(x of texto ){
+        
+    }
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 function decriptoCesar(){
     let alfabeto=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
@@ -62,17 +65,44 @@ function decriptoCesar(){
 
 }
 
+
+
+
+//finalizado
 function cripto64(){
+    let input=document.getElementById('campo-input').value;
+    let novo=btoa(input);
+    document.getElementById('campo-output').value=novo;
+
     
 
 }
 
+//finalizado
 function decripto64(){
+    let input=document.getElementById('campo-input').value;
+    let novo=atob(input);
+    document.getElementById('campo-output').value=novo;
 
 }
+
+
+
+
+
+
+
+
+/*
+Y2Fpbw==
 
 function validaCampo(){
+    if(parseInt(document.querySelector('#inc').value)
+    
+    
+    )
 
 
 
-}
+};
+*/
